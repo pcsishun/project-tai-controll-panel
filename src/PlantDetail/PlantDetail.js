46db1391df28db3572d2658f17e3f370944f5257
mikeGLA <mikecommand_conquer@hotmail.com>
2022-04-04 22:28:22 +0700

    Update Fodder Plantation Page Upper Part

--- src/PlantDetail/PlantDetail.js
+++ src/PlantDetail/PlantDetail.js
@@ -1,13 +1,76 @@
 import "./PlantDetail.css"
+import BackButton from "../images/Button Back.png"
+import LeafIcon from "../images/LeafIcon.png"
+import HumidityImage from "../images/humidity 1.png"
+import TempImage from "../images/thermometer 1.png"
+import PlantAgeImage from "../images/carbon_soil-moisture-field.png"
+
 
 const PlantDetail = () => {
     return (
         <>
             <div className="PlantDetail-header">
-                
+                <div className="PlantDetail-BackButton">
+                    <img src={BackButton} width="44" height="44"/>
+                </div>
+                <div className="PlantDetail-PlantCount">
+                    <p className="">800 Plants</p>
+                </div>
+                <div className="PlantDetail-ProductName">
+                    <div>
+                        <div className="ProductName">
+                            <p>Fodder Plantation</p>
+                        </div>
+                        <div className="ProductName-Location">
+                            <p>Sansai, Chiang Mai, Thailand</p>
+                        </div>
+                    </div>
+                    <div className="ProductName-LeafIcon">
+                        <img src={LeafIcon} width="80" height="80"/>
+                    </div>
+                </div>
+                <div className="PlantDetail-SensorMonitor">
+                    <div className="Monitor-Humidity">
+                        <div className="Humidity-Image">
+                            <img className="" src={HumidityImage} height="26" width="26"/>
+                        </div>
+                        <div className="Humidity-Text">
+                            <p>Humidity</p>
+                        </div>
+                        <div className="Humidity-Var">
+                            <p>20 %</p>
+                        </div>
+                    </div>
+                    <div className="Monitor-Temp">
+                        <div className="Temp-Image">
+                            <img className="" src={TempImage} height="26" width="26"/>
+                        </div>
+                        <div className="Temp-Text">
+                            <p>Temperature</p>
+                        </div>
+                        <div className="Temp-Var">
+                            <p>22&#x2103;</p>
+                        </div>
+
+                    </div>
+                    <div className="Monitor-PlantAge">
+                        <div className="PlantAge-Image">
+                            <img className="" src={PlantAgeImage} height="26" width="26"/>
+                        </div>
+                        <div className="PlantAge-Text">
+                            <p>Temperature</p>
+                        </div>
+                        <div className="PlantAge-Var">
+                            <p>7 Days</p>
+                        </div>
+
+                    </div>
+                </div>
             </div>
 
-            <div className="PlantDetail-body"></div>
+            <div className="PlantDetail-body">
+
+            </div>
         </>
     );
 }
