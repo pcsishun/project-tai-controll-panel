8866847c249a19fc905571a57810fa7b7f7d9b14
mikeGLA <mikecommand_conquer@hotmail.com>
2022-04-09 00:41:45 +0700

    Update Consumption Page

--- src/Consumption/Consumption.js
+++ src/Consumption/Consumption.js
@@ -2,8 +2,18 @@ import "./Consumption.css"
 import  BackButton from "../images/Button Back.png"
 import WaterDrop from "../images/waterdropblue.png"
 import ThunderIcon from "../images/thunder-2 2.png"
+import {useState, useEffect} from 'react';
+
+
 
 const Consumption = () => {
+
+    const [dayColor, setdayColor] = useState("Date-Day-Active");
+
+    const [monthColor, setmonthColor] = useState("Date-Month");
+
+    const [yearColor, setyearColor] = useState("Date-Year");
+
     return (
         <>
             <div className="Consumption-header">
@@ -29,26 +39,32 @@ const Consumption = () => {
 
             <div className="Consumption-body">
                 <div className="Consumption-Date">
-                <span className="Consumption-Date-Day">
-                Day
+                <span className="Consumption-Date-Day"><button className={dayColor} onClick={()=> {setdayColor("Date-Day-Active")}}>
+                Day</button>
                 </span>
-                <span className="Consumption-Date-Month">
-                Month
+                <span className="Consumption-Date-Month"><button className="Date-Month">
+                Month</button>
                 </span>
-                <span className="Consumption-Date-Month">
-                Year
+                <span className="Consumption-Date-Year"><button className="Date-Year">
+                Year</button>
                 </span>
                 </div>
 
                 <div className="Consumption-Cost">
                     <div className="Consumption-WaterPrice">
                         <div className="WaterPrice-HalfCircle"><span>14<br/>THB</span></div>
+                        <div className="WaterPrice-Image"> <img src={WaterDrop} width="28" height="38"/></div>
+                        <div className="WaterPrice-Label">Water Price</div>
                     </div>
 
                     <div className="Consumption-ElectricPrice">
-                        <div className="ElectricPrice-HalfCircle"></div>
+                        <div className="ElectricPrice-HalfCircle"><span>34<br/>THB</span></div>
+                        <div className="ElectricPrice-Image"> <img src={ThunderIcon} width="30px" height="32px"/></div>
+                        <div className="ElectricPrice-Label">Electricity Price</div>
                     </div>
                 </div>
+
+                <div className="Consumption-Resource-Graph"></div>
 
             </div>
         </>
